c8f4d54b7791d21acba2b1df6c78fadf9421a2be
jimmyberg <jimmyberg.kim@gmail.com>
2022-07-08 00:40:10 +0900

    question 004 추가

---
+++ src/main/java/me/jimmyberg/algorithm/doit/chapter03/Question004.java
@@ -0,0 +1,39 @@
+package me.jimmyberg.algorithm.doit.chapter03;
+
+import java.util.Scanner;
+
+/**
+ * 문제 004. 구간 합 구하기 2
+ * - N x N 개의 숫자 표에서 (x1, y1) ~ (x2, y2) 까지의 구간 합 구하기
+ * - N x N 숫자 입력 표 A
+ * - N x N 각 구간별 합산 표 D
+ * - 구간별 합산 = 해당 구간을 둘러싼 구간 합을 빼면 시작점부터 끝점까지의 구간 합!!
+ */
+public class Question004 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int quiz = sc.nextInt();
+        System.out.println("size = " + size);
+        System.out.println("quiz = " + quiz);
+
+        int[][] D = new int[size + 1][size + 1];
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                int n = sc.nextInt();
+                D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + n;
+            }
+        }
+
+        for (int q = 0; q < quiz; q++) {
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+
+            int result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
+            System.out.println("result = " + result);
+        }
+
+    }
+}
