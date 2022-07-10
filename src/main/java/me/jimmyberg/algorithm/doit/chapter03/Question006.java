f98a599a0ad26a91b6cac13087348dfe8857c1d2
jimmyberg <jimmyberg.kim@gmail.com>
2022-07-11 00:27:00 +0900

    question 006 추가

---
+++ src/main/java/me/jimmyberg/algorithm/doit/chapter03/Question006.java
@@ -0,0 +1,42 @@
+package me.jimmyberg.algorithm.doit.chapter03;
+
+import me.jimmyberg.algorithm.common.CommonUtil;
+
+import java.util.Scanner;
+
+/**
+ * 문제 006. 연속된 자연수의 합 구하기
+ * - 자연수 N 의 연속된 자연수의 합의 경우의 수를 구하기
+ * - N : 15 인 경우
+ *  - 1 + 2 + 3 + 4 + 5
+ *  - 4 + 5 + 6
+ *  - 7 + 8
+ *  - 15
+ * - 총 : 4
+ */
+public class Question006 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int start = 1, end = 1, sum = 1, count = 0;
+
+        while (end < N) {
+            if (sum < N) {
+                end++;
+                sum += end;
+            }
+            if (sum > N) {
+                sum -= start;
+                start++;
+            }
+            if (sum == N) {
+                end++;
+                sum += end;
+                count++;
+            }
+        }
+
+        System.out.println("count = " + count);
+    }
+}
