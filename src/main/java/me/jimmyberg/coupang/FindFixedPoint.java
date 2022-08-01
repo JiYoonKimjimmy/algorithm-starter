f16fcb12a7e0a3e7e12cbc9a4592e6722395bbea
jimmyberg <jimmyberg.kim@gmail.com>
2022-08-01 10:13:01 +0900

    FindFixedPoint 수정

--- src/main/java/me/jimmyberg/coupang/FindFixedPoint.java
+++ src/main/java/me/jimmyberg/coupang/FindFixedPoint.java
@@ -1,54 +1,48 @@
 package me.jimmyberg.coupang;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * 문제. 고정점 찾기
  * - 숫자로 된 배열에서 index 와 value 가 동일한 `고정점` 를 찾는다.
+ * - `고정점` 없는 경우엔, `-1` 출력
  * - O(logN) 시간 복잡도를 준수하기 위해 선형 탐색이 아닌 방식으로 탐색한다.
  *
  * [Key Point]
  * - `퀵 정렬` 을 활용하여 분할 정복 방식으로 탐색!
  * - 결과 반환 방식 및 임계 종료 방식 고려
+ *
+ * [Sample]
+ * - input : -16, -15, 1, 3, 9, 6
+ * - out : 3
+ *
+ * - input : -16, -15, 1, 3, 9, 6
+ * - out : -1
  */
 public class FindFixedPoint {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(-16, -15, 1, 3, 9, 5);
-
-        System.out.println(sort(list).stream().map(String::valueOf).collect(Collectors.joining(", ")));
+        List<Integer> list = Arrays.asList(-16, -15, 1, 2, 3, 6);
+        System.out.println("findFixedPoint = " + findFixedPoint(list, 0, list.size()));
     }
 
-    public static List<Integer> sort(List<Integer> list) {
-        if (list.size() <= 1) return list;
+    public static int findFixedPoint(List<Integer> list, int start, int end) {
+        if (start >= end) return -1;
 
-        int pivot = list.size() / 2;
+        int pivot = (start + end) / 2;
         int pivot_num = list.get(pivot);
+        System.out.print("pivot = " + pivot);
+        System.out.println(", pivot_num = " + pivot_num);
 
-        List<Integer> lesser = new ArrayList<>(), equal = new ArrayList<>(), greater = new ArrayList<>();
-
-        for (int i = 0; i < list.size(); i++) {
-            int num = list.get(i);
-
-            if (num < pivot_num) {
-                // lesser group
-                lesser.add(num);
-            } else if (num > pivot_num) {
-                // greater group
-                greater.add(num);
-            } else {
-                 // equal group
-                equal.add(num);
-            }
+        if (pivot < pivot_num) {
+            // pivot 보다 pivot_num 이 작으면, 왼쪽 탐색
+            return findFixedPoint(list, start, pivot);
+        } else if (pivot > pivot_num) {
+            // pivot 보다 pivot_num 이 크면, 오른쪽 탐색
+            return findFixedPoint(list, pivot + 1, end);
+        } else {
+            // 같으면, 해당 pivot 반환
+            return pivot_num;
         }
-
-        return Stream
-                .of(sort(lesser), equal, sort(greater))
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
     }
 }
