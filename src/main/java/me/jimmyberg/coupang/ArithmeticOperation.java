2174246f328f2350b6574c287d21e0afe0a5039e
jimmyberg <jimmyberg.kim@gmail.com>
2022-08-05 22:50:54 +0900

    괄호 연산자 처리 추가

--- src/main/java/me/jimmyberg/coupang/ArithmeticOperation.java
+++ src/main/java/me/jimmyberg/coupang/ArithmeticOperation.java
@@ -6,7 +6,6 @@ import java.util.Stack;
 /**
  * 문제. 사칙 연산 구현하기
  * - 수식 문자열에 맞게 사칙 연산 구현하기
- *
  * [Key Point]
  * - '중위 표기법'은 '후위 표기법'으로 변경해야한다!
  * - '후위 표기법'? 연산자를 피연사자들 뒤에 위치
@@ -19,14 +18,15 @@ import java.util.Stack;
  *      3. 스택이 빌 때까지 나머지 연산자를 `pop` 한다.
  * - `*` or `/` 같은 경우는, `+` or `-` 보다 연산 우선 순위가 높은 경우를 고려 필요
  * - '여는 괄호 `(`' 는 스택에 `push` 한다.
- * - '닫는 괄호 `)`' 는 스택에 있는 연산자를 모두 `pop` 한다.
- *
+ * - '닫는 괄호 `)`' 는 '여는 괄호' 가 나올 때까지 스택에 있는 연산자를 모두 `pop` 한다.
  * [Sample]
  * input : "1 + 2-3 + 5-2 * 5-3"
  * output : -8
  */
 public class ArithmeticOperation {
     static HashMap<Character, Integer> op_map = new HashMap<>() {{
+        put('(', 1);
+        put(')', 1);
         put('+', 2);
         put('-', 2);
         put('*', 3);
@@ -34,7 +34,9 @@ public class ArithmeticOperation {
     }};
 
     public static void main(String[] args) {
-        String str = "1 + 2-3 + 5-2 * 5-3";     // -8
+//        String str = "1 + 2-3 + 5-2 * 5-3";     // -8
+//        String str = "(1 + 2) * (3 + 4)";       // 21
+        String str = "( 1 * ( 2 + 3 )) - 4";      // 1
         System.out.println("str = " + str);
 
         Stack<Character> op_stack = new Stack<>();
@@ -43,24 +45,35 @@ public class ArithmeticOperation {
             if (Character.isDigit(c)) {
                 sb.append(c);
             } else {
-                if (op_stack.isEmpty() || op_map.get(op_stack.peek()) < op_map.get(c)) {
+                if (op_stack.isEmpty() || op_map.get(op_stack.peek()) < op_map.get(c) || '(' == c) {
+                    // stack 비어있거나, stack 마지막 연산자가 현재 연산자보다 작거나, 현재 연산자가 '여는 괄호' 인 경우
                     op_stack.push(c);
                 } else {
-                    char last_op = op_stack.peek();
                     // stack 마지막 연산자가 현재 연산자보다 같거나 큰 경우
-                    if (op_map.get(last_op) > op_map.get(c)) {
-                        // stack 마지막 연산자가 현재 연산자보다 큰 경우
+                    if (')' == c) {
+                        // '닫는 괄호' 인 경우, '여는 괄호' 까지 모두 `pop` 처리
+                        while ('(' != op_stack.peek()) {
+                            sb.append(op_stack.pop());
+                        }
+                        op_stack.pop();
+                    } else {
+                        char last_op = op_stack.peek();
+                        if (op_map.get(last_op) > op_map.get(c)) {
+                            // stack 마지막 연산자가 현재 연산자보다 큰 경우
+                            sb.append(op_stack.pop());
+                        }
                         sb.append(op_stack.pop());
+                        op_stack.push(c);
                     }
-                    sb.append(op_stack.pop());
-                    op_stack.push(c);
                 }
             }
         }
 
+        // stack 에 남은 연산자 모두 `pop`처리
         while (!op_stack.isEmpty()) {
             sb.append(op_stack.pop());
         }
+
         System.out.println("sb = " + sb);
         System.out.println("result = " + calculate(sb.toString()));
     }
