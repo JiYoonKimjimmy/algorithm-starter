bebecd75894f82f5ce8b03a76195ea725e851b74
jimmyberg <jimmyberg.kim@gmail.com>
2022-09-06 17:08:10 +0900

    Question 027 수정

--- src/main/java/me/jimmyberg/algorithm/doit/chapter05/Question027.java
+++ src/main/java/me/jimmyberg/algorithm/doit/chapter05/Question027.java
@@ -2,8 +2,10 @@ package me.jimmyberg.algorithm.doit.chapter05;
 
 import kotlin.Pair;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.stream.Stream;
 
 /**
  * 문제 027. 미로 탐색하기
@@ -16,16 +18,23 @@ import java.util.Queue;
  * - `BFS` 알고리즘은 해당 깊이에서 탐색을 마친 후, 다음 깊이로 넘어가기 때문에 이 문제에 더 어울리는 기법이다!
  */
 public class Question027 {
+//    static int[][] matrix = {
+//            {0, 0, 0, 0, 0, 0, 0, 0},
+//            {0, 1, 1, 0, 1, 1, 0, 0},
+//            {0, 1, 1, 0, 1, 1, 0, 0},
+//            {0, 1, 1, 1, 1, 1, 1, 0},
+//            {0, 1, 1, 1, 1, 0, 1, 0},
+//            {0, 0, 0, 0, 0, 0, 0, 0}
+//    };
     static int[][] matrix = {
             {0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 1, 1, 0, 1, 1, 0, 0},
-            {0, 1, 1, 0, 1, 1, 0, 0},
-            {0, 1, 1, 1, 1, 1, 1, 0},
-            {0, 1, 1, 1, 1, 0, 1, 0},
+            {0, 1, 0, 1, 1, 1, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 0},
+            {0, 1, 0, 1, 0, 1, 1, 0},
+            {0, 1, 1, 1, 0, 1, 1, 0},
             {0, 0, 0, 0, 0, 0, 0, 0}
     };
     static boolean[][] visited;
-    static int depth = 0;
 
     public static void main(String[] args) {
         int N = 4, M = 6;
@@ -42,7 +51,7 @@ public class Question027 {
 
         bfs(1, 1);
 
-        System.out.println("result = " + depth);
+        System.out.println("result = " + (matrix[x][y] - 1));
     }
 
     private static void bfs(int i, int j) {
@@ -54,24 +63,28 @@ public class Question027 {
             int x = node.getFirst();
             int y = node.getSecond();
 
-            int up = matrix[x][y - 1];
-            int down = matrix[x][y + 1];
-            int left = matrix[x - 1][y];
-            int right = matrix[x + 1][y];
+            int up    = matrix[x - 1][y];
+            int down  = matrix[x + 1][y];
+            int left  = matrix[x][y - 1];
+            int right = matrix[x][y + 1];
 
-            if (up    == 1 && !visited[x][y - 1]) queue.add(new Pair<>(x, y - 1));
-            if (down  == 1 && !visited[x][y + 1]) queue.add(new Pair<>(x, y + 1));
-            if (left  == 1 && !visited[x - 1][y]) queue.add(new Pair<>(x - 1, y));
-            if (right == 1 && !visited[x + 1][y]) queue.add(new Pair<>(x + 1, y));
+            if (up    == 1 && !visited[x - 1][y]) queue.add(new Pair<>(x - 1, y));
+            if (down  == 1 && !visited[x + 1][y]) queue.add(new Pair<>(x + 1, y));
+            if (left  == 1 && !visited[x][y - 1]) queue.add(new Pair<>(x, y - 1));
+            if (right == 1 && !visited[x][y + 1]) queue.add(new Pair<>(x, y + 1));
 
-            if (!visited[x][y]) {
+            if (matrix[x][y] == 1 && !visited[x][y]) {
                 visited[x][y] = true;
-                depth++;
+                matrix[x][y] = max(up, down, left, right) + 1;
             }
 
             if (x == 4 && y == 6) {
                 break;
             }
         }
+    }
+
+    private static int max(int up, int down, int left, int right) {
+        return Stream.of(up, down, left, right).max(Comparator.comparingInt(a -> a)).get();
     }
 }
