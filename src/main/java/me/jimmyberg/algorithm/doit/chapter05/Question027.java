1cf9f6b4b4e89b37b80ce5498a4155e1acd5f7b7
jimmyberg <jimmyberg.kim@gmail.com>
2022-09-06 21:20:53 +0900

    Question 027 solution.2 추가

--- src/main/java/me/jimmyberg/algorithm/doit/chapter05/Question027.java
+++ src/main/java/me/jimmyberg/algorithm/doit/chapter05/Question027.java
@@ -12,29 +12,29 @@ import java.util.stream.Stream;
  * - 미로의 각 칸에 들어 있는 숫자 중 `1`은 이동할 수 있는 칸, `0`은 이동할 수 없는 칸을 나타낸다.
  * - 한칸에서 다른 칸으로 이동할 때는 인접한 칸으로 이동할 수 있다.
  * - 위 조건에 맞게 시작 위치에서 도착 위치까지의 지나가야하는 칸의 갯수를 구한다.
- *
+ * <p>
  * [Key Point]
  * - 완전 탐색 기법을 활용하지만, 몇 번째 깊이에서 원하는 값을 찾을 수 있는 `BFS` 알고리즘을을 활용!
  * - `BFS` 알고리즘은 해당 깊이에서 탐색을 마친 후, 다음 깊이로 넘어가기 때문에 이 문제에 더 어울리는 기법이다!
  * - !! 매우 중요 !! 현재 노드의 깊이를 연결 노드 중 제일 큰 값 + 1 하여 저장 !!
  */
 public class Question027 {
-//    static int[][] matrix = {
-//            {0, 0, 0, 0, 0, 0, 0, 0},
-//            {0, 1, 1, 0, 1, 1, 0, 0},
-//            {0, 1, 1, 0, 1, 1, 0, 0},
-//            {0, 1, 1, 1, 1, 1, 1, 0},
-//            {0, 1, 1, 1, 1, 0, 1, 0},
-//            {0, 0, 0, 0, 0, 0, 0, 0}
-//    };
     static int[][] matrix = {
             {0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 1, 0, 1, 1, 1, 1, 0},
-            {0, 1, 0, 1, 0, 1, 0, 0},
-            {0, 1, 0, 1, 0, 1, 1, 0},
-            {0, 1, 1, 1, 0, 1, 1, 0},
+            {0, 1, 1, 0, 1, 1, 0, 0},
+            {0, 1, 1, 0, 1, 1, 0, 0},
+            {0, 1, 1, 1, 1, 1, 1, 0},
+            {0, 1, 1, 1, 1, 0, 1, 0},
             {0, 0, 0, 0, 0, 0, 0, 0}
     };
+//    static int[][] matrix = {
+//            {0, 0, 0, 0, 0, 0, 0, 0},
+//            {0, 1, 0, 1, 1, 1, 1, 0},
+//            {0, 1, 0, 1, 0, 1, 0, 0},
+//            {0, 1, 0, 1, 0, 1, 1, 0},
+//            {0, 1, 1, 1, 0, 1, 1, 0},
+//            {0, 0, 0, 0, 0, 0, 0, 0}
+//    };
     static boolean[][] visited;
 
     public static void main(String[] args) {
@@ -50,9 +50,9 @@ public class Question027 {
             System.out.println();
         }
 
-        bfs(1, 1);
+        bfs2(1, 1);
 
-        System.out.println("result = " + (matrix[x][y] - 1));
+        System.out.println("result = " + (matrix[x][y]));
     }
 
     private static void bfs(int i, int j) {
@@ -64,14 +64,14 @@ public class Question027 {
             int x = node.getFirst();
             int y = node.getSecond();
 
-            int up    = matrix[x - 1][y];
-            int down  = matrix[x + 1][y];
-            int left  = matrix[x][y - 1];
+            int up = matrix[x - 1][y];
+            int down = matrix[x + 1][y];
+            int left = matrix[x][y - 1];
             int right = matrix[x][y + 1];
 
-            if (up    == 1 && !visited[x - 1][y]) queue.add(new Pair<>(x - 1, y));
-            if (down  == 1 && !visited[x + 1][y]) queue.add(new Pair<>(x + 1, y));
-            if (left  == 1 && !visited[x][y - 1]) queue.add(new Pair<>(x, y - 1));
+            if (up == 1 && !visited[x - 1][y]) queue.add(new Pair<>(x - 1, y));
+            if (down == 1 && !visited[x + 1][y]) queue.add(new Pair<>(x + 1, y));
+            if (left == 1 && !visited[x][y - 1]) queue.add(new Pair<>(x, y - 1));
             if (right == 1 && !visited[x][y + 1]) queue.add(new Pair<>(x, y + 1));
 
             if (matrix[x][y] == 1 && !visited[x][y]) {
@@ -87,5 +87,50 @@ public class Question027 {
 
     private static int max(int up, int down, int left, int right) {
         return Stream.of(up, down, left, right).max(Comparator.comparingInt(a -> a)).get();
+    }
+
+    /**
+     * Solution.2
+     * - 우 -> 하 -> 좌 -> 상 순으로 인접 노드를 탐색
+     * - 탐색한 노드 순으로 depth 저장
+     * - 탐색 노드의 depth = 현재 노드 depth + 1
+     */
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+    private static void bfs2(int i, int j) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{i, j});
+        visited[i][j] = true;
+
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            int now_x = now[0];
+            int now_y = now[1];
+
+            for (int k = 0; k < dx.length; k++) {
+                int x = now_x + dx[k];  // 현재 노드 기준 인접 노드 x index
+                int y = now_y + dy[k];  // 현재 노드 기준 인접 노드 y index
+
+                if (x > 0 && y > 0 && x <= 4 && y <= 6) {
+                    System.out.print("now_x = " + now_x);
+                    System.out.print(" , now_y = " + now_y);
+                    System.out.print(" , matrix[" + x + "][" + y + "] = " + matrix[x][y]);
+                    System.out.print(" , visited[" + x + "][" + y + "] = " + visited[x][y]);
+
+                    // 인접 노드 적합성 확인 : x, y 의 index 가 `0` 이상 이면서, 기준 N, M 이상 아닌 경우
+                    if (matrix[x][y] != 0 && !visited[x][y]) {
+                        // 인접 노드가 `0` 아니면서, 방문하지 않은 노드인 경우
+                        visited[x][y] = true;
+                        matrix[x][y] = matrix[now_x][now_y] + 1;
+                        System.out.print(" , matrix[" + x + "][" + y + "] = " + matrix[x][y]);
+                        queue.add(new int[]{x, y});
+                    }
+                    System.out.println();
+                }
+            }
+        }
+
+
     }
 }
