dd1ab1c8bfecdb1f6f52edafcb0e347c949e3480
jimmyberg <jimmyberg.kim@gmail.com>
2022-09-18 03:47:51 +0900

    never 2022 demo test 추가

--- src/main/java/me/jimmyberg/naver2022/DemoTest.java
+++ src/main/java/me/jimmyberg/naver2022/DemoTest.java
@@ -2,7 +2,11 @@ package me.jimmyberg.naver2022;
 
 import java.util.Arrays;
 
-public class Demo {
+/**
+ * NAVER 2022 코딩 테스트 Demo Test
+ * - 1 ~ 100,000 자리 수의 주어진 순차적인 정수 배열에서 빠져있는 숫자 중 제일 작은 수 구하기
+ */
+public class DemoTest {
     public static void main(String[] args) {
         int[] A = {1, 3, 6, 4, 1, 2, -1};
 //        int[] A = {1, 2, 3};
