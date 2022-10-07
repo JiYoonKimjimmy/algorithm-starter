b6413b603cb8176d3c4421af5c9c4c312b7b503a
jimmyberg <jimmyberg.kim@gmail.com>
2022-10-07 19:40:04 +0900

    SieveOfEratosthenes algorithm 추가

---
+++ src/main/java/me/jimmyberg/algorithm/basic/SieveOfEratosthenes.java
@@ -0,0 +1,32 @@
+package me.jimmyberg.algorithm.basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SieveOfEratosthenes {
+    public static void main(String[] args) {
+        int N = 30;
+        int[] A = new int[N];
+
+        for (int i = 1; i <= 30; i++) {
+            A[i - 1] = i;
+        }
+
+        for (int i = 1; i < A.length; i++) {
+            int t = A[i];
+            for (int j = i + 1; j < A.length; j++) {
+                if (t != 1 && A[j] % t == 0) {
+                    A[j] = 1;
+                }
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int j : A) {
+            if (j != 1) {
+                list.add(j);
+            }
+        }
+        System.out.println("list = " + list);
+    }
+}
