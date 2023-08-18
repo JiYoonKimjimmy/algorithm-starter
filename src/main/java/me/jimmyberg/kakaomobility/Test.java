0429b7184e7a0ebeebba569e585361ac3c5758b1
jimmyberg <jimmyberg.kim@gmail.com>
2023-08-18 19:52:46 +0900

    add: Sample Test code

---
+++ src/main/java/me/jimmyberg/kakaomobility/Test.java
@@ -0,0 +1,15 @@
+package me.jimmyberg.kakaomobility;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.solution();
+    }
+
+    private void solution() {
+        int answer = 0;
+        System.out.println("answer = " + answer);
+    }
+
+}
---
+++ src/main/kotlin/me/jimmyberg/algorithm/kakaomobility/Test.kt
@@ -0,0 +1,15 @@
+package me.jimmyberg.algorithm.kakaomobility
+
+fun main() {
+    Test().solution()
+}
+
+class Test {
+
+    fun solution() {
+        val answer = 0
+
+        println("answer = $answer")
+    }
+
+}
