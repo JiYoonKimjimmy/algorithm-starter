ecb8fa6f217b38c18ed5f1ca27718a461104b070
jimmyberg <jimmyberg.kim@gmail.com>
2025-09-01 14:08:01 +0900

    feat: 추가

---
+++ src/test/kotlin/me/jimmyberg/algorithm/moloco/year2025/Test01.kt
@@ -0,0 +1,12 @@
+package me.jimmyberg.algorithm.moloco.year2025
+
+import org.junit.jupiter.api.Test
+
+class Test01 {
+
+    @Test
+    fun test() {
+
+    }
+
+}
---
+++ src/test/kotlin/me/jimmyberg/algorithm/moloco/year2025/Test04.kt
@@ -0,0 +1,12 @@
+package me.jimmyberg.algorithm.moloco.year2025
+
+import org.junit.jupiter.api.Test
+
+class Test04 {
+
+    @Test
+    fun test() {
+        
+    }
+
+}
